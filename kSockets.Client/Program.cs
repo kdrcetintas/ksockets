f8961dab92ad816370f43cc4c9e7a7aa518f9cec
Kadir Çetintaş <mert@mail.com>
2018-07-06 12:02:12 +0300

    fixes

--- kSockets.Client/Program.cs
+++ kSockets.Client/Program.cs
@@ -1,52 +1,40 @@
-﻿using kdrcts.kSockChannel;
-using Newtonsoft.Json;
-using System;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Xml;
-using System.Xml.Serialization;
+﻿using System;
+using kdrcts.kSockChannel;
 
 namespace kSockets.Client
 {
-    class Program
+    internal class Program
     {
-        public class sampleObject
+        private static void Main(string[] args)
         {
-            public string Name;
-            public string Message;
-        }
-
-        static void Main(string[] args)
-        {
-            kSocketChannel mySocket = new kSocketChannel();
-            mySocket.OutgoingConnected += (Socket) =>
+            var mySocket = new kSocketChannel();
+            mySocket.OutgoingConnected += Socket =>
             {
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.WriteLine("Client is connected to outgoing socket");
-                byte[] byteobject = kdrcts.kSockChannel.kSocketChannelHelpers.ObjectSerializer<sampleObject>(new sampleObject()
+                var byteobject = kSocketChannelHelpers.ObjectSerializer(new sampleObject
                 {
                     Name = "kdrcts",
                     Message = "Hello, I'm client"
                 }).StringToBytes();
                 mySocket.SendData(Socket, byteobject);
             };
-            mySocket.OutgoingDisconnected += (Socket) =>
+            mySocket.OutgoingDisconnected += Socket =>
             {
                 Console.ForegroundColor = ConsoleColor.DarkGray;
                 Console.WriteLine("Client is disconnected");
             };
-            mySocket.ChannelLog += (kSocketChannel.ChannelLogTypes Log, string Message) =>
+            mySocket.ChannelLog += (Log, Message) =>
             {
                 Console.ForegroundColor = ConsoleColor.DarkGray;
                 Console.WriteLine("Channel Log: {0} - {1}", Log.ToString(), Message);
             };
-            mySocket.ChannelError += (kSocketChannel.ChannelErrorTypes Error, string Message, Exception Ex, System.Net.Sockets.Socket Socket) =>
+            mySocket.ChannelError += (Error, Message, Ex, Socket) =>
             {
                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("Channel Error: {0} - {1}", Error.ToString(), Message);
             };
-            mySocket.DataReceived += (kSocketTransferItem Item, System.Net.Sockets.Socket Socket) =>
+            mySocket.DataReceived += (Item, Socket) =>
             {
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine(Item.TransferedData.ToArray().BytesToString());
@@ -54,6 +42,12 @@ namespace kSockets.Client
 
             mySocket.Connect("127.0.0.1", 1920);
             Console.ReadLine();
+        }
+
+        public class sampleObject
+        {
+            public string Message;
+            public string Name;
         }
     }
 }
--- kSockets.Server/Program.cs
+++ kSockets.Server/Program.cs
@@ -1,64 +1,59 @@
-﻿using kdrcts.kSockChannel;
-using System;
-using System.IO;
+﻿using System;
 using System.Linq;
 using System.Net;
-using System.Reflection;
-using System.Xml;
-using System.Xml.Serialization;
+using kdrcts.kSockChannel;
 
 namespace kSockets.Server
 {
-    class Program
+    internal class Program
     {
-        public class sampleObject
+        private static void Main(string[] args)
         {
-            public string Name { get; set; }
-            public string Message { get; set; }
-        }
-
-        static void Main(string[] args)
-        {
-            kSocketChannel mySocket = new kSocketChannel();
+            var mySocket = new kSocketChannel();
             mySocket.SetSocketBufferSize(4096);
-            
-            mySocket.IncomingConnected += (Socket) =>
+
+            mySocket.IncomingConnected += Socket =>
             {
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
-                Console.WriteLine("Connection is comed from {0} and Handle {1}", (Socket.RemoteEndPoint as IPEndPoint).Address.ToString(), Socket.Handle.ToInt32());
+                Console.WriteLine("Connection is comed from {0} and Handle {1}",
+                    (Socket.RemoteEndPoint as IPEndPoint).Address.ToString(), Socket.Handle.ToInt32());
             };
-            mySocket.IncomingDisconnected += (Socket) =>
+            mySocket.IncomingDisconnected += Socket =>
             {
                 Console.ForegroundColor = ConsoleColor.DarkGray;
-                Console.WriteLine("Connection is disconnected from {0} and Handle {1}", (Socket.RemoteEndPoint as IPEndPoint).Address.ToString(), Socket.Handle.ToInt32());
+                Console.WriteLine("Connection is disconnected from {0} and Handle {1}",
+                    (Socket.RemoteEndPoint as IPEndPoint).Address.ToString(), Socket.Handle.ToInt32());
             };
 
-            mySocket.ChannelLog += (kSocketChannel.ChannelLogTypes Log, string Message) =>
+            mySocket.ChannelLog += (Log, Message) =>
             {
                 Console.ForegroundColor = ConsoleColor.DarkGray;
                 Console.WriteLine("Channel Log: {0} - {1}", Log.ToString(), Message);
             };
-            mySocket.ChannelError += (kSocketChannel.ChannelErrorTypes Error, string Message, Exception Ex, System.Net.Sockets.Socket Socket) =>
+            mySocket.ChannelError += (Error, Message, Ex, Socket) =>
             {
                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("Channel Log: {0} - {1}", Error.ToString(), Message);
             };
-            mySocket.DataReceived += (kSocketTransferItem Item, System.Net.Sockets.Socket Socket) =>
+            mySocket.DataReceived += (Item, Socket) =>
             {
                 Console.ForegroundColor = ConsoleColor.Blue;
-                sampleObject receivedObject = kdrcts.kSockChannel.kSocketChannelHelpers.ObjectDeserializer<sampleObject>(Item.TransferedData.ToArray());
+                var receivedObject =
+                    kSocketChannelHelpers.ObjectDeserializer<sampleObject>(Item.TransferedData.ToArray());
                 Console.WriteLine("{0} says: {1}", receivedObject.Name, receivedObject.Message);
                 mySocket.SendData(Socket, "You are welcome".StringToBytes());
             };
             mySocket.Listen(1920);
             listenKeys:
-            string b = Console.ReadLine();
+            var b = Console.ReadLine();
             if (b == "l")
-            {
                 Console.WriteLine("Active Connection List, Count: {0} \n", mySocket.GiveConnectedSockets().Count());
-            }
             goto listenKeys;
         }
-
+        public class sampleObject
+        {
+            public string Name { get; set; }
+            public string Message { get; set; }
+        }
     }
 }
